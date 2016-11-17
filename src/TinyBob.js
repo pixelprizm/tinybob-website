d5a845c7b15e6cd8f115798d71b1790cead5efb0
Eric Gauderman <eric.gauderman@gmail.com>
2016-11-17 10:30:06 -0800

    Fonts, footer section, images

--- src/TinyBob.js
+++ src/TinyBob.js
@@ -4,6 +4,7 @@ import './css/TinyBob.css';
 const placeholderSrcWide = 'https://bytesizemoments.com/wp-content/uploads/2014/04/placeholder3.png';
 const placeholderSrcSquare = 'http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png';
 const kenneyLogoSrc = 'http://kenney.nl/data/img/logo.png';
+const gamepipeLogoSrc = '/viterbi-gamepipe-logo.png';
 
 class TinyBob extends Component {
   render() {
@@ -11,13 +12,13 @@ class TinyBob extends Component {
       <div className="TinyBob">
         <div className="jumbotron TinyBob-header">
           <div className="container">
-            <h1 className="TinyBob-header-title">
+            <h1 className="TinyBob-header-title TinyBob-title">
               <span className="TinyBob-big-letter">T</span>
               INY
               <span className="TinyBob-big-letter">B</span>
               OB
             </h1>
-            <h2 className="TinyBob-header-subtitle">
+            <h2 className="TinyBob-header-subtitle TinyBob-title">
               A fast-paced racing platformer game
             </h2>
           </div>
@@ -25,7 +26,7 @@ class TinyBob extends Component {
 
         <div className="TinyBob-game-section">
           <div className="container">
-            <h1 className="TinyBob-section-title">
+            <h1 className="TinyBob-section-title TinyBob-title">
               <span className="TinyBob-big-letter">G</span>
               AMEPLAY
             </h1>
@@ -55,7 +56,7 @@ class TinyBob extends Component {
 
         <div className="TinyBob-team-section">
           <div className="container">
-            <h1 className="TinyBob-section-title">
+            <h1 className="TinyBob-section-title TinyBob-title">
               <span className="TinyBob-big-letter">T</span>
               EAM
             </h1>
@@ -67,15 +68,21 @@ class TinyBob extends Component {
 
         <div className="TinyBob-footer-section">
           <div className="container">
-            <div className="TinyBob-footer-row">
-              <img alt="Kenney logo" src={kenneyLogoSrc} className="TinyBob-footer-logo"/>
-              <p>
-                We thank <a href="http://kenney.nl">kenney.nl</a> for providing free art assets that were used in the making of TinyBob.
+            <div className="row TinyBob-footer-row">
+              <div className="col-sm-4 col-md-3 TinyBob-footer-logo-positioning">
+                <img alt="Kenney logo" src={kenneyLogoSrc} className="TinyBob-footer-logo"/>
+              </div>
+              <p className="col-sm-8 col-md-9 TinyBob-footer-p">
+                We thank <a href="http://kenney.nl">kenney.nl</a> for providing free art assets which were used in the making of TinyBob.
               </p>
-              <div className="TinyBob-clearfix"/>
             </div>
-            <div className="TinyBob-footer-row">
-              <p>
+            <div className="row TinyBob-footer-row">
+              <div className="col-sm-4 col-md-3 TinyBob-footer-logo-positioning">
+                <div className="TinyBob-footer-logo-backing">
+                  <img alt="USC Viterbi GamePipe logo" src={gamepipeLogoSrc} className="TinyBob-footer-logo"/>
+                </div>
+              </div>
+              <p className="col-sm-8 col-md-9 TinyBob-footer-p">
                 Made for the CSCI-523 Networked Games class at USC in fall 2016.
               </p>
             </div>
@@ -90,18 +97,21 @@ class TinyBob extends Component {
     const people = [
       {
         name: 'Nick Wong',
-        position: 'Team Lead, Networking',
+        position: 'Team Lead, Networking engineer',
         bio: 'Some sentences about the team member Nick Wong will go here, but for now this is just placeholder text.',
+        img: '/eric.jpg',
       },
       {
         name: 'Matt Carey',
-        position: 'Gameplay, Art Assets',
+        position: 'Gameplay engineer, 3D Art Assets',
         bio: 'Some sentences about the team member Matt Carey will go here, but for now this is just placeholder text.',
+        img: '/eric.jpg',
       },
       {
         name: 'Eric Gauderman',
-        position: 'Gameplay, Balancing',
+        position: 'Gameplay engineer, Balancing',
         bio: 'Some sentences about the team member Eric Gauderman will go here, but for now this is just placeholder text.',
+        img: '/eric.jpg',
       },
     ];
     const person = people[index];
@@ -110,9 +120,9 @@ class TinyBob extends Component {
       <div className="col-sm-4">
         <div className="TinyBob-team-column">
           <div className="TinyBob-team-image-container">
-            <img alt="Team member" src={placeholderSrcSquare} className="TinyBob-team-image"/>
+            <img alt="Team member" src={person.img} className="TinyBob-team-image"/>
           </div>
-          <h2 className="TinyBob-team-name">
+          <h2 className="TinyBob-team-name TinyBob-title">
             {person.name}
           </h2>
           <div className="TinyBob-team-position">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TinyBob from './TinyBob';
-import './css/index.css';
+
+// (It's important to import Bootstrap first so that our CSS will override it.)
 import './css/bootstrap/css/bootstrap.css';
+import './css/index.css';
 
 ReactDOM.render(
   <TinyBob />,
